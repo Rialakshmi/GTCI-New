105ea6e3ef2684c0d0c986c365c3e271c7e35090
Nagalakshmi Chithambaranathan <nagawinbagath@gmail.com>
2020-10-23 21:50:37 -0700

    Create MinSizeSubArraySum.java

---
+++ SlidingWindow/MinSizeSubArraySum.java
@@ -0,0 +1,49 @@
+/**
+Problem Statement #
+
+Given an array of positive numbers and a positive number ‘S’, find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’. Return 0, if no such subarray exists.
+
+Example 1:
+
+Input: [2, 1, 5, 2, 3, 2], S=7 
+Output: 2
+Explanation: The smallest subarray with a sum great than or equal to '7' is [5, 2].
+
+Example 2:
+
+Input: [2, 1, 5, 2, 8], S=7 
+Output: 1
+Explanation: The smallest subarray with a sum greater than or equal to '7' is [8].
+
+Example 3:
+
+Input: [3, 4, 1, 1, 6], S=8 
+Output: 3
+Explanation: Smallest subarrays with a sum greater than or equal to '8' are [3, 4, 1] or [1, 1, 6].
+**/
+class MinSizeSubArraySum {
+  public static int findMinSubArray(int S, int[] arr) {
+    int min=Integer.MAX_VALUE;
+    int sum=0;
+    int j=0;
+    for(int i=0;i<arr.length;i++) {
+      sum+=arr[i];
+      while(sum>=S) {
+        min=Math.min(min,i-j+1);
+        sum-=arr[j++];
+      }
+      
+    }
+
+    return min==Integer.MAX_VALUE?0:min;
+  }
+
+  public static void main(String[] args) {
+    int result = MinSizeSubArraySum.findMinSubArray(7, new int[] { 2, 1, 5, 2, 3, 2 });
+    System.out.println("Smallest subarray length: " + result);
+    result = MinSizeSubArraySum.findMinSubArray(7, new int[] { 2, 1, 5, 2, 8 });
+    System.out.println("Smallest subarray length: " + result);
+    result = MinSizeSubArraySum.findMinSubArray(8, new int[] { 3, 4, 1, 1, 6 });
+    System.out.println("Smallest subarray length: " + result);
+  }
+}
