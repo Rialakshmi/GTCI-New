8460a3e300dfa863a171296e5dbbcba97629961d
Nagalakshmi Chithambaranathan <nagawinbagath@gmail.com>
2020-11-03 11:41:43 -0800

    Create 4FindDuplicate.java

---
+++ Cyclic Sort/4FindDuplicate.java
@@ -0,0 +1,39 @@
+/****
+Problem Statement #
+
+We are given an unsorted array containing ‘n+1’ numbers taken from the range 1 to ‘n’. The array has only one duplicate but it can be repeated multiple times. Find that duplicate number without using any extra space. You are, however, allowed to modify the input array.
+
+Example 1:
+
+Input: [1, 4, 4, 3, 2]
+Output: 4
+
+Example 2:
+
+Input: [2, 1, 3, 3, 5, 4]
+Output: 3
+
+Example 3:
+
+Input: [2, 4, 1, 4, 4]
+Output: 4
+****/
+class FindDuplicate {
+
+  public static int findNumber(int[] nums) {
+    for(int i=0;i<nums.length;i++) {
+      while(nums[i]!=i+1) {
+        if(nums[nums[i]-1]==nums[i])
+        return nums[i];
+        swap(nums,i,nums[i]-1);
+      }
+    }
+    return -1;
+  }
+
+  public static void swap(int[] nums,int i1,int i2) {
+    int temp=nums[i1];
+    nums[i1]=nums[i2];
+    nums[i2]=temp;
+  }
+}
