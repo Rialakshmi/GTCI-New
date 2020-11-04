e1be18fd0eb8da56a0c0157a0f4efd52989fc696
Nagalakshmi Chithambaranathan <nagawinbagath@gmail.com>
2020-11-04 10:40:25 -0800

    Update 3ReverseEveryKElements.java

--- In place Reversal Of Linked List/3ReverseEveryKElements.java
+++ In place Reversal Of Linked List/3ReverseEveryKElements.java
@@ -1,3 +1,14 @@
+/*****
+Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+
+k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+Follow up:
+
+    Could you solve the problem in O(1) extra memory space?
+    You may not alter the values in the list's nodes, only nodes itself may be changed.
+
+*****/
 import java.util.*;
 
 class ListNode {
